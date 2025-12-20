141c281a31ab0749198df29d5b755a0aa8eb02bb
Pauline-Oraro <hiltrapauline@gmail.com>
2025-12-20 16:45:37 +0300

    switching products

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -10,11 +10,28 @@ Title: macbook pro M3 16 inch 2024
 
 
 import { useGLTF, useTexture } from '@react-three/drei'
+import useMacbookStore from '../../store';
+import { useEffect } from 'react';
+import { noChangeParts } from '../../constants';
+import { Color } from 'three';
 
-export default function MackbookModel14(props) {
-  const { nodes, materials } = useGLTF('/models/macbook-14-transformed.glb')
+export default function MacbookModel14(props) {
+   const {color} = useMacbookStore()
+
+  const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb')
 
   const texture = useTexture('/screen.png');
+
+  useEffect(() => {
+    scene.traverse((child) =>{
+      if(child.isMesh){
+        if(!noChangeParts.includes(child.name)){
+          child.material.color = new Color(color);
+        }
+      }
+    })
+  }, [color, scene])
+  
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} rotation={[Math.PI / 2, 0, 0]} />
